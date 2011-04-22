7f98d239b5c766dc2453d96c898c54bfdf7483c1
max <max@localhost>
2011-04-22 10:52:53 +0800

    fix for train / tag scripts

--- tag.sh
+++ tag.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+MALLET_HOME=/root/mallet/mallet-2.0.6
+
 echo "Usage: tag.sh [MODEL FILE] [TEST FILE]"
 if [ "$2" = '' ]
 then
     exit
 fi
-java -cp "/root/mallet/mallet-2.0.6/class:/root/mallet/mallet-2.0.6/lib/mallet-deps.jar" \
+
+java -cp "$MALLET_HOME/class:$MALLET_HOME/lib/mallet-deps.jar" \
 cc.mallet.fst.SimpleTagger \
 --model-file "$1" "$2"
--- train.sh
+++ train.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+MALLET_HOME=/root/mallet/mallet-2.0.6
+
 echo "Usage: train.sh [Training File] [Thread Number] [Output Model File]"
-if [ "$2" = '' ]
+if [ "$3" = '' ]
 then
     exit
 fi
-java -cp "/root/mallet/mallet-2.0.6/class:/root/mallet/mallet-2.0.6/lib/mallet-deps.jar" \
+
+java -cp "$MALLET_HOME/class:$MALLET_HOME/lib/mallet-deps.jar" \
 cc.mallet.fst.SimpleTagger \
 --train true --model-file "$3" --threads $2 "$1"
 
